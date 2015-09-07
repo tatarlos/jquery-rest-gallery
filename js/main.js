42b46cb8f08a66bc6ffedd224754edbef583e7c7
tatarlos <haochen.liu6487@1708c009.mediadesign.internal>
2015-09-07 14:27:49 +1200

    templates
    sdfaf

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function($){
 
+	var dataURL ="data/data.json",
+	dataModel ={};
 
+	function loadData(){
+		 $.getJSON(dataURL, function(data){
+		 	console.log(data);
+		 	dataModel.images = data;
+		 });
+	}
+	loadData();
 });
